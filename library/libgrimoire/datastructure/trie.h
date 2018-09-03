afb600dde876d42dbe7340784ebf2777e40aa0ba
Kuroneko <kuroneko4764@gmail.com>
2018-09-03 15:44:04 +0900

    trie...
    유진이 보고싶다...

--- library/libgrimoire/datastructure/trie.h
+++ library/libgrimoire/datastructure/trie.h
@@ -2,13 +2,14 @@
 #define _TRIE_
 
 #include <stdint.h>
+#include <libgrimoire/datastructure/trie_layer.h>
 
 typedef struct trie trie_t;
 
 struct trie {
 	int (*insert)(trie_t * this, void * src, int src_len);
 	int (*remove)(trie_t * this, void * src, int src_len);
-	void * (*find)(trie_t * this, void * src, int src_len);
+	trie_layer_t * (*find)(trie_t * this, void * src, int src_len);
 	void (*dump)(trie_t * this);
 };
 
